463c8f1b0a77aabd89366714522b02032a626f3d
Joel Alcaraz <joelchristopheralcaraz@gmail.com>
2022-11-03 12:12:59 -0700

    Add post and put route

--- routes/companies.js
+++ routes/companies.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 
 const db = require("../db");
 
-const { NotFoundError } = require("../expressError");
+const { NotFoundError, BadRequestError } = require("../expressError");
 
 const router = Router();
 
@@ -36,11 +36,63 @@ router.get("/:code", async function (req, res) {
     [code]
   );
 
-  const company = results.row[0];
+  const company = results.rows[0];
 
   if (!company) throw new NotFoundError();
 
   return res.json({ company });
+});
+
+/**
+ * POST /companies
+ * Adds a company to DB
+ * Takes in JSON like {code, name, description}
+ * Returns obj of new company JSON like {company: {code, name, description}}
+ */
+
+router.post("/", async function (req, res) {
+  if(req.body === undefined) throw new BadRequestError();
+  // What happens if input is optional?
+
+  const {code, name, description} = req.body;
+
+  const results = await db.query(
+    `INSERT INTO companies (code, name, description)
+        VALUES ($1, $2, $3)
+        RETURNING code, name, description`,
+    [code, name, description]
+  );
+
+  const company = results.rows[0];
+
+  return res.status(201).json({ company });
+});
+
+/**
+ * PUT /companies/[code]
+ * Edits existing company
+ * Takes in JSON like {name, description}
+ * Returns obj of updated company JSON like {company: {code, name, description}}
+ * Should return 404 if company cannot be found
+ */
+
+router.put("/:code", async function (req, res) {
+  if(req.body === undefined) throw new BadRequestError();
+
+  const {name, description} = req.body;
+
+  const results = await db.query(
+    `UPDATE companies
+        SET name=$1,
+            description=$2
+        WHERE code = $3
+        RETURNING code, name, description`,
+    [name, description, req.params.code],
+  );
+
+  const company = results.rows[0];
+
+  return res.json({ company })
 });
 
 module.exports = router;
